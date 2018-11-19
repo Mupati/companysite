16d7e26b1400b96a1f7b4d7a00abe2acdf9b7504
Mupati <scientificgh@gmail.com>
2018-11-19 09:54:25 +0000

    changed login link

--- src/components/Ekheader.vue
+++ src/components/Ekheader.vue
@@ -15,7 +15,7 @@
                     </div>
                     <!-- Main Menus Wrapper -->
                     <div class="nav-menus-wrapper">
-                        <router-link to="/login" target="_blank" class="nav-button btn-pill bg-mat-green align-to-right">Login</router-link>
+                        <router-link to="/" target="_blank" class="nav-button btn-pill bg-mat-green align-to-right">Login</router-link>
                         <ul class="nav-menu align-to-right" id="nav">
                             <li class="active"><a href="#home">Home</a></li>
                             <li><a href="#about">About Us</a></li>
